d188a11889aa258f5cc3b1fc6778a937e668f645
shibinaa <112463732+shibinaa@users.noreply.github.com>
2022-09-06 14:31:19 +0530

    Update settings.py

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=zt*6kukg(-tmkyy+!4_kbz9w7480o89zwr=&c-@zgwsn*al3p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','todoapp0011.herokuapp.com']
 
